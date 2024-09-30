7e58dda93ef2ce6ea16dc242dfeb5bf5a94fedde
AlekseyLAS1912 <159919281+AlekseyLAS1912@users.noreply.github.com>
2024-09-30 22:41:41 +1200

    Add files via upload

---
+++ M1DZ2.py
@@ -0,0 +1,6 @@
+total_tasks = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = hours_spent / total_tasks
+print('Название курса:', course_name, 'Всего задач:', total_tasks, 'Затрачено часов:', hours_spent,
+      'Среднее время выполнения:', time_for_one_task)
