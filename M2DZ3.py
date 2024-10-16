8ac375701ea9048b538d355acb6e32984e8551ca
AlekseyLAS1912 <159919281+AlekseyLAS1912@users.noreply.github.com>
2024-10-16 22:06:19 +1200

    Update M2DZ3.py

--- M2DZ3.py
+++ M2DZ3.py
@@ -1,12 +1,15 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-for i in range(2, len(numbers) + 1):
+for i in numbers:
     for j in range(2, i):
         if i % j == 0:
             not_primes.append(i)
             break
     else:
         primes.append(i)
+    if i == 1:
+        primes.remove(i) and not_primes.remove(i)
+
 print('Primes: ', primes)
 print('Not primes: ', not_primes)
